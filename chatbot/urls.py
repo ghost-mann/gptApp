63ef79f823ce94c7a222d5c46a477484ded3ddad
ghost-mann <iamaob8@protonmail.com>
2023-09-27 17:57:17 +0300

    connected models and database

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+# path specifies each url as a path
+
+#views
 from . import views
 urlpatterns = [
     path('', views.chatbot, name='chatbot')
--- chatbot/views.py
+++ chatbot/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import openai
+
+openai_api_key = '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'
+openai.api_key = openai_api_key
+
+def ask_openai(message):
+    response = openai.Completion.create(
+        model = "text-davinci-003",
+        prompt = message,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+
+    answer = response.choices[0].text.strip()
+    return answer
+
+
 
 # Create your views here.
 def chatbot(request):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = 'hey this is my response'
+        return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatbot.html')
-
-
-
